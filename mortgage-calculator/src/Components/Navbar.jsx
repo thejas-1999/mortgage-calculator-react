c155215efccd9eab2fd9825b2529fdc395efe183
thejas-1999 <thejask074@gmail.com>
2024-03-14 11:06:22 +0530

    create a simple navbar

--- mortgage-calculator/src/Components/Navbar.jsx
+++ mortgage-calculator/src/Components/Navbar.jsx
@@ -1,4 +1,16 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Bank of React
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 };
 export default Navbar;
